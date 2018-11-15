c2f07450540cf7073460082cee39cfbd7089e684
lrllzzr <43448423+lrllzzr@users.noreply.github.com>
2018-11-16 00:45:03 +0900

    3

--- Nast.js
+++ Nast.js
@@ -12,36 +12,6 @@
 document.title = 'Welcome!'
 
 
-
-     // $(document).ready(function() {
-     //
-     //      $.ajax({
-     //
-     //           type: 'GET',
-     //
-     //           dataType: 'jsonp',
-     //
-     //           data: {'name': 'kimyeonsuk'},
-     //
-     //           url: 'https://www.youtube.com/watch?v=KsF_hdjWJjo',
-     //
-     //           // jsonp 값을 전달할 때 사용되는 파라미터 변수명
-     //
-     //           // 이 속성을 생략하면 callback 파라미터 변수명으로 전달된다.
-     //
-     //           jsonp: 'stone',
-     //
-     //           success:function(json) {
-     //
-     //                $('.main_content a').html(json.data.name);
-     //
-     //           }
-     //
-     //      });
-     //
-     // });
-
-
   $( function() {
     $( "#tabs" ).tabs({
       event: "mouseover"
@@ -53,11 +23,11 @@ function get_list(name){
 }
 
 function SetTitle() {
-  document.write('<div id="title">Astronomy World</div>');
+  document.write('<div id="title_ast">Astronomy World</div>');
 }
 
 var banner = ['Main','Sun','Moon','About Universe','Theory of relativity','Awesome stories'];
-var sun = ['태양','태양의 구성','태양의 수명','태양계','햇빛의 나이는?']
+var sun = ['태양','태양의 구성','태양의 수명','태양계','햇빛의 나이는?','달이 1px 이라면']
 var universe = ['우주에서 가장 큰 별','금성의 하루는 일년?','가장 큰 다이아몬드?','우주 엘레베이터']
 var moon = ['달','달의 탄생','달은 인공물질?']
 var theory = ['상대성이론이란?','시공간의 왜곡','쌍둥이 역설','빛보다 빠른 우주선?']
